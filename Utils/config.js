c1f0f5d924f4ffe07c428eb93e56c4c226126581
Godswill Edet <gimmex1@gmail.com>
2021-11-24 20:02:10 +0100

    changed api

--- Utils/config.js
+++ Utils/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000" || "https://readabl-e.herokuapp.com",
+  baseURL: "https://readabl-e.herokuapp.com",
   headers: {
     Authorization: "GregData",
     "Access-Control-Allow-Credentials": true,
