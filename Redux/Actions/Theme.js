6ba4e10457255259b599248d32b68b50fe8110a3
Godswill Edet <gimmex1@gmail.com>
2021-12-01 10:27:50 +0100

    Removed theme reducer from redux

--- Redux/Actions/Theme.js
+++
@@ -1,6 +0,0 @@
-export const TOGGLE_THEME = "TOGGLE_THEME";
-
-export const toggleTheme = (mode) => ({
-  type: TOGGLE_THEME,
-  payload: mode,
-});
--- Redux/store/rootReducer.js
+++ Redux/store/rootReducer.js
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux";
-import ThemeReducer from "./../Reducers/Theme";
 import postsReducer from "./../Reducers/Posts";
 import categoriesReducer from "./../Reducers/Categories";
 import commentsReducer from "./../Reducers/Comments";
 
 const rootReducer = combineReducers({
-  ThemeReducer,
   postsReducer,
   categoriesReducer,
   commentsReducer,
